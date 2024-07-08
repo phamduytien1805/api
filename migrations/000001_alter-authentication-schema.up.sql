880b33ab7bb748e9749bb3bdac33787728c30cbc
PHAM DUY TIEN <duytien@MacBook-Pro-cua-PHAM.local>
2024-07-08 22:41:23 +0700

    feat: add migration

---
+++ migrations/000001_alter-authentication-schema.up.sql
@@ -0,0 +1,34 @@
+CREATE TABLE "users" (
+  "id" uuid PRIMARY KEY,
+  "username" VARCHAR(32) NOT NULL UNIQUE,
+  "email" VARCHAR(254) NOT NULL UNIQUE,
+  "email_verified" bool NOT NULL DEFAULT false,
+  "created_at" timestamptz NOT NULL DEFAULT (now()),
+  "state" SMALLINT NOT NULL,
+  "stated_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "user_credentials" (
+  "user_id" uuid NOT NULL,
+  "credential" varchar NOT NULL,
+  "salt" varchar NOT NULL,
+  UNIQUE(user_id)
+);
+
+CREATE TABLE "user_social_tokens" (
+  "user_id" uuid,
+  "provider_type" varchar(50),
+  "access_token" varchar,
+  "refresh_token" varchar,
+  "expires" timestamptz,
+  "created_at" timestamptz NOT NULL DEFAULT (now()),
+  "updated_at" timestamptz NOT NULL DEFAULT (now()),
+  PRIMARY KEY (user_id, provider_type)
+);
+
+ALTER TABLE "user_social_tokens" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+ALTER TABLE "user_credentials" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+COMMENT ON COLUMN "users"."state" IS 'enum defined in code';
+
+COMMENT ON COLUMN "user_social_tokens"."provider_type" IS 'enum defined in code';
