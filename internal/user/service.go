1327a9b212cbbb736502d3a0547621ff34de6447
PHAM DUY TIEN <duytien@MacBook-Pro-cua-PHAM.local>
2024-07-20 22:59:41 +0700

    feat: create user credential

--- internal/user/service.go
+++ internal/user/service.go
@@ -2,18 +2,22 @@ package user
 
 import (
 	"context"
+	"encoding/hex"
 
 	data_access "github.com/phamduytien1805/internal/data-access"
+	"github.com/phamduytien1805/pkg/hash_generator"
 	"github.com/phamduytien1805/pkg/id_generator"
 )
 
 type UserServiceImpl struct {
-	store data_access.Store
+	store   data_access.Store
+	hashGen *hash_generator.Argon2idHash
 }
 
-func NewUserServiceImpl(store data_access.Store) *UserServiceImpl {
+func NewUserServiceImpl(store data_access.Store, hashGen *hash_generator.Argon2idHash) *UserServiceImpl {
 	return &UserServiceImpl{
-		store: store,
+		store:   store,
+		hashGen: hashGen,
 	}
 }
 
@@ -30,11 +34,15 @@ func (svc *UserServiceImpl) CreateUserWithCredential(ctx context.Context, user *
 			EmailVerified: false,
 			State:         1,
 		},
-		AfterCreate: func(user data_access.User) error {
-			_, err := svc.store.CreateUserCredential(ctx, data_access.CreateUserCredentialParams{
-				UserID:     user.ID,
-				Credential: "",
-				Salt:       "user.Salt",
+		AfterCreate: func(createdUser data_access.User) error {
+			hashSalt, err := svc.hashGen.GenerateHash([]byte(user.Credential), nil)
+			if err != nil {
+				return err
+			}
+			_, err = svc.store.CreateUserCredential(ctx, data_access.CreateUserCredentialParams{
+				UserID:     createdUser.ID,
+				Credential: hex.EncodeToString(hashSalt.Hash),
+				Salt:       hex.EncodeToString(hashSalt.Salt),
 			})
 			return err
 		},
